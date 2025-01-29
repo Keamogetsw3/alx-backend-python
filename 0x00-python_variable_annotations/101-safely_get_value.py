0bd86e3d50db92101bff1ba7333cbe324608ff28
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-29 16:28:04 +0200

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python3
-"""
-This module defines a function `safely_get_value` that safely retrieves
-a value from a dictionary. If the key is not found, it returns a default
-value instead.
-"""
+""" More involved type annotations  """
+from typing import Mapping, Any, Sequence, Union, TypeVar
 
-from typing import Mapping, Any, Union, TypeVar
 
-# Type variable to handle any type for default value
 T = TypeVar('T')
 
 
-def safely_get_value(
-        dct: Mapping[Any, Any], key: Any,
-        default: Union[T, None]) -> Union[Any, T]:
-    """
-    Retrieve the value for a given key from a dictionary. If the key is not
-    found, return the provided default value.
-
-    Parameters:
-    dct (Mapping[Any, Any]): A dictionary with keys and values of any type.
-    key (Any): The key to look up in the dictionary.
-    default (Union[T, None]): The value to return if the key is not found.
-    Can be any type (specified by T) or None.
-
-    Returns:
-    Union[Any, T]: The value corresponding to the key if found,
-    or the default value (which can be any type or None) if the key is not found.
-    """
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """ Safely get value """
     if key in dct:
         return dct[key]
     else:
