4ed21f01c6c11e9474abd1c5cd1392f33e4da102
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-29 21:14:28 +0200

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-'''Module for creating an asynchronous task.
-
-This module defines a function that creates and returns an asyncio Task 
-for executing `wait_random` asynchronously.
+'''Task 4's module.
 '''
-
 import asyncio
 from typing import List
 
 
-# Importing wait_random from the previous module
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Creates and returns an asyncio Task for `wait_random`.
+    '''Executes task_wait_random n times.
     '''
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
+    return sorted(wait_times)
