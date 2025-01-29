471894c1035c633fadb96662aa906d2e97279c89
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2025-01-29 19:00:54 +0000

    Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Module for creating an asynchronous task.
+
+This module defines a function that creates and returns an asyncio Task 
+for executing `wait_random` asynchronously.
+'''
+
+import asyncio
+
+# Importing wait_random from the previous module
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates and returns an asyncio Task for `wait_random`.
+
+    This function wraps `wait_random(max_delay)` inside an `asyncio.Task`,
+    which allows it to run concurrently with other asynchronous tasks.
+
+    Args:
+        max_delay (int): The maximum delay for the `wait_random` coroutine.
+
+    Returns:
+        asyncio.Task: A task object representing the execution of `wait_random`.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
