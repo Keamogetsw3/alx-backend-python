89aa362a77187b987b44bdee92eec2f7ad3b5cc2
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-29 16:20:07 +0200

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 """
-This module defines a type-annotated function `to_str` that converts a
-floating-point number to its string representation.
+Write a type-annotated function floor which takes a float n as argument
+and returns the floor of the float.
 """
+import math
 
 
-def to_str(n: float) -> int:
-    """
-    Convert a floating-point number to its string representation.
-
-    Parameters:
-    n (float): The floating-point number to convert.
-
-    Returns:
-    int: The string representation of the input float.
-    """
-    return int(n)
+def floor(n: float) -> int:
+    """ Returns: floor of a floating number """
+    return int(math.floor(n))
